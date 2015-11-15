8bb29cae8ce3ec4daddc8eedee314ba7f06d8c40
tjt263 <tjt263@gmail.com>
2015-11-15 10:25:39 +0000

    First commit

---
+++ iso2usb.1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+if [[ "$#" == 0 ]]
+then
+	clear
+	echo " "
+	echo "Interactive Mode.."
+	sleep 0.75
+	echo " "
+	sleep 0.75
+	echo "Desired ISO Image; File Path: "
+	sleep 0.75
+	read ISO
+	sleep 0.75
+	echo " "
+	sleep 0.75
+	echo "Desired USB Disk; Directory Path: "
+	sleep 0.75
+	read DISK
+	sleep 0.75
+################## [[ $1 == "help" || "-h" || "--help" ||  ]] ##################
+	echo " "
+	sleep 0.75
+	echo "Imaging the USB drive. This can take some time. >30min is not unusual."
+	echo "No further feedback will be provided until the process is complete."
+	echo "Please be patient."
+	sleep 0.75
+	echo " "
+	sudo dd if="$ISO" of="$DISK" bs=1M
+	sleep 0.75
+	echo " "
+elif [[ "$#" == 2 ]]
+then
+	echo " "
+	sleep 0.75
+	echo "Imaging the USB drive. This can take some time. >30min is not unusual."
+	echo "No further feedback will be provided until the process is complete."
+	echo "Please be patient."
+	sleep 0.75
+	echo " "
+	sudo dd if="$1" of="$2" bs=1M
+	sleep 0.75
+	echo " "
+else
+	clear
+	echo " "
+	echo "ISO2USB v0.1beta;"
+	echo "Create a bootable USB drive."
+	echo " "
+	sleep 0.75
+	echo "Usage:   	./iso2usb.sh  [ISO]... [DISK]..."
+	echo "         	./iso2usb.sh  [INPUT]. [OUTPUT]."
+	sleep 0.75
+	echo " "
+	echo "Example: 	./iso2usb.sh  '/user/directory/image-file-1.0-amd64.iso' '/dev/disk'"
+	echo "         	./iso2usb.sh  '/user/directory/image-file-1.0-amd64.iso' '/dev/sdb6'"
+	sleep 0.75
+	echo " "
+fi
---
+++ iso2usb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [[ "$#" == 0 ]]
+then
+	clear
+	echo " "
+	echo "Interactive Mode.."
+	echo " "
+	echo "Desired ISO Image; File Path: "
+	read ISO
+	echo " "
+	echo "Desired USB Disk; Directory Path: "
+	read DISK
+################## [[ $1 == "help" || "-h" || "--help" ||  ]] ##################
+	echo " "
+	echo "Imaging the USB drive. This can take some time. >30min is not unusual."
+	echo "No further feedback will be provided until the process is complete."
+	echo "Please be patient."
+	echo " "
+	sudo dd if="$ISO" of="$DISK" bs=1M
+	
+	echo " "
+elif [[ "$#" == 2 ]]
+then
+	echo " "
+	echo "Imaging the USB drive. This can take some time. >30min is not unusual."
+	echo "No further feedback will be provided until the process is complete."
+	echo "Please be patient."
+	echo " "
+	sudo dd if="$1" of="$2" bs=1M
+	
+	echo " "
+else
+	clear
+	echo " "
+	echo "ISO2USB v0.1beta;"
+	echo "Create a bootable USB drive."
+	echo " "
+	echo "Usage:   	./iso2usb.sh  [ISO]... [DISK]..."
+	echo "         	./iso2usb.sh  [INPUT]. [OUTPUT]."
+	
+	echo " "
+	echo "Example: 	./iso2usb.sh  '/user/directory/image-file-1.0-amd64.iso' '/dev/disk'"
+	echo "         	./iso2usb.sh  '/user/directory/image-file-1.0-amd64.iso' '/dev/sdb6'"
+	
+	echo " "
+fi
